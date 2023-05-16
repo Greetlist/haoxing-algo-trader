b98cd06b7a87264e0f6be2a64c319e021cb1a232
Greetlist <454281389@qq.com>
2023-05-16 16:37:04 +0800

    add logger

--- trader.py
+++ trader.py
@@ -5,11 +5,15 @@ import pandas as pd
 import time
 import signal
 import datetime as dt
+import sys
+import logging
+import subprocess as sub
 
 class HPITrader:
     def __init__(self, config):
         self.config = config
         self.today_str = dt.datetime.now().strftime("%Y%m%d")
+        self.init_logger()
         self.targetpos_filename = self.config["target_pos_file_template"].format(self.today_str)
         self.idx_filename = self.config["idx_file_template"].format(self.today_str, self.today_str)
 
@@ -20,25 +24,41 @@ class HPITrader:
 
         self.trade_api = HttpClient(
             self.config["server_addr"],
-            user=self.config["user"],
-            password=self.config["password"],
+            user=self.config["trader_user"],
+            password=self.config["trader_password"],
         )
         login_res = self.trade_api.login()
+        self.logger.info(login_res)
         if login_res["reset_password"] == 1:
+            self.logger.info("Need Reset Password")
             self.trade_api.update_password(self.config["trader_password"], self.config["trader_password"])
         self.cli_token = login_res["token"]
 
         trade_account_list = self.trade_api.get_tradeaccount()
         for item in trade_account_list:
-            print(item)
+            self.logger.info("trade account: ", item)
             if item["account_id"] == config["fund_account"]:
                 if item["status"] == 0:
+                    self.logger.info("Need Activate Account")
                     self.trade_api.activate_account(config["fund_account"], config["fund_account_password"])
                 self.account_id = config["fund_account"]
 
+        sys.exit(1)
         self.call_back = TraderCallback(self.config, token)
         self.call_back.init()
         self.call_back.start()
+
+    def init_logger(self):
+        sub.check_call("mkdir -p {}".format(self.config["log_base_dir"]), shell=True)
+        log_file_name = self.config["log_name_template"].format(self.today_str)
+        self.logger = logging.getLogger(log_file_name)
+        self.logger.setLevel(logging.INFO)
+        if not self.logger.handlers:
+            formatter = logging.Formatter("[%(asctime)s] | %(levelname)s | [%(filename)s:%(lineno)s] | %(message)s")
+            file_handler = logging.FileHandler(os.path.join(self.config["log_base_dir"], log_file_name))
+            file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
 
     def reload_order_index(self):
         if os.path.exists(self.idx_filename):
@@ -57,18 +77,18 @@ class HPITrader:
         while not self.stop:
             if os.path.exists(self.targetpos_filename):
                 file_stat = os.stat(self.targetpos_filename)
-                print(file_stat.st_ctime)
+                self.logger.info(file_stat.st_ctime)
                 if file_stat.st_ctime <= self.last_change_time:
-                    print("TargetPos File not change, continue")
+                    self.logger.info("TargetPos File not change, continue")
                     time.sleep(1)
                     continue
 
                 target_pos_df = pd.read_csv(self.targetpos_filename)[self.start_order_index:]
                 task_list = self.gen_total_task_list(target_pos_df)
                 if len(task_list) > 0:
-                    print(task_list)
+                    self.logger.info(task_list)
                     #res = self.trade_api.create_tasks(task_list)
-                    #print(res)
+                    #self.logger.info(res)
                     if True:
                         self.start_order_index += len(task_list)
                         self.save_idx()
@@ -79,7 +99,7 @@ class HPITrader:
         for item in df.to_dict("records"):
             time_list = item["ExtraData"].split(";")
             if len(time_list) < 2:
-                print("Invalid record item: ", item)
+                self.logger.info("Invalid record item: ", item)
                 return []
             start_time_str, end_time_str = time_list[0], time_list[1]
             single_task = StockTask(
@@ -103,8 +123,8 @@ class HPITrader:
 
     def convert_time(self, time_str):
         d = dt.datetime.strptime(time_str, "%H:%M:%S")
-        print(d.hour, d.minute, d.second)
-        return d.hour * 10000 + d.minute * 1000 + d.second
+        self.logger.info(d.hour, d.minute, d.second)
+        return d.hour * 10000 + d.minute * 100 + d.second
 
     def save_idx(self):
         with open(self.idx_filename, "w") as f:
